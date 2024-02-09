737d47a9a6e5604d03405083229e9fedc34957d3
Anwarat Gurung <81597440+anw-g01@users.noreply.github.com>
2024-02-09 02:48:04 +0000

    Add files via upload

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time, json, os
 
+WEB_URL = "https://orteil.dashnet.org/experiments/cookie/"
+
 
 def clear_screen():
     """
@@ -13,22 +15,22 @@ def clear_screen():
 class CookieClickerBot:
 
     def __init__(self, bot_name: str, cookies_target: int, toggle_nums=True):
-        self.name = bot_name  # bot name to refer to in final message text file
-        self.target = cookies_target  # number of generated cookies until game completion
-        self._toggle_numbers(toggle_nums)  # toggle on/off numbers display
-        self.url = "https://orteil.dashnet.org/experiments/cookie/"  # Chrome website URL for cookie clicker game
-        self.driver = self._create_webdriver()  # Selenium WebDriver
-        # ********* gameplay statistics ********* #
-        self.start_time = time.time()  # starting time of instantiation
-        self.webdriver_clicks = 0  # running count of all clicks made by webdriver
-        self.bought_upgrades = 0  # running count of no. of bought upgrades
-        self.cookies_spent = 0  # running count of cookies spent on upgrades
-        self.total_cookies = 0  # running count of all time cookies generated
-        self.text_file = f"{self.name}_stats.txt"  # text file name containing the final stats after a given run
-        # ********* attributes to be scraped ********* #
-        self.money = 0  # count of current cookies available
-        self.clicks_rate = 0  # current cookies/second value obtained from upgrades
-        self.upgrades = self._scrape_upgrades()  # current prices for each upgrade (dictionary)
+        self.name = bot_name                            # bot name to refer to in final message text file
+        self.target = cookies_target                    # number of generated cookies until game completion
+        self._toggle_numbers(toggle_nums)               # toggle on/off numbers display
+        self.url = WEB_URL                              # Chrome website URL for cookie clicker game
+        self.driver = self._create_webdriver()          # Selenium WebDriver
+        # --- gameplay statistics --- #
+        self.start_time = time.time()                   # starting time of instantiation
+        self.webdriver_clicks = 0                       # running count of all clicks made by webdriver
+        self.bought_upgrades = 0                        # running count of no. of bought upgrades
+        self.cookies_spent = 0                          # running count of cookies spent on upgrades
+        self.total_cookies = 0                          # running count of all time cookies generated
+        self.text_file = f"{self.name}_stats.txt"       # text file name containing the final stats after a given run
+        # --- attributes to be scraped --- #
+        self.money = 0                                  # count of current cookies available
+        self.clicks_rate = 0                            # current cookies/second value obtained from upgrades
+        self.upgrades = self._scrape_upgrades()         # current prices for each upgrade (dictionary)
 
     def _toggle_numbers(self, flag=True) -> None:
         """If set to False, toggle numbers is turned off by clicking 'Numbers Off' button in browser."""
@@ -41,10 +43,10 @@ class CookieClickerBot:
 
     def _create_webdriver(self) -> webdriver:
         """Returns a WebDriver instance for controlling a Chrome web browser."""
-        opts = webdriver.ChromeOptions()  # configure options
-        opts.add_experimental_option("detach", True)  # webdriver session to be kept open until closed by command
-        driver = webdriver.Chrome(options=opts)  # create webdriver instance
-        driver.get(url=self.url)  # open specified URL in a Chrome browser
+        opts = webdriver.ChromeOptions()                # configure options
+        opts.add_experimental_option("detach", True)    # webdriver session to be kept open until closed by command
+        driver = webdriver.Chrome(options=opts)         # create webdriver instance
+        driver.get(url=self.url)                        # open specified URL in a Chrome browser
         return driver
 
     def _scrape_upgrades(self) -> dict:
@@ -57,9 +59,9 @@ class CookieClickerBot:
         prices = [int(ele.text.split()[-1].replace(",", "")) for ele in elements]  # changing prices of all upgrades
         upgrades = {}
         for i in range(1, len(elements) + 1):
-            upgrades[names[i - 1]] = {  # use upgrade name as the key
-                "upgrade no.": i,  # hierarchy of upgrades
-                "price": prices[i - 1]  # updating price
+            upgrades[names[i - 1]] = {          # use upgrade name as the key
+                "upgrade no.": i,               # hierarchy of upgrades
+                "price": prices[i - 1]          # updating price
             }
         return upgrades
 
@@ -104,9 +106,9 @@ class CookieClickerBot:
 
     def digital_elapsed_time(self) -> str:
         """Returns the current elapsed time in digital padded string format."""
-        elapsed_time = self.calc_elapsed_time()  # elapsed time in seconds
-        minutes = round(elapsed_time // 60)
-        seconds = round(elapsed_time % 60)  # take remainder as seconds
+        elapsed_time = self.calc_elapsed_time()     # elapsed time in seconds
+        minutes = round(elapsed_time // 60)         # integer division for minutes
+        seconds = round(elapsed_time % 60)          # take remainder as seconds
         return f"{minutes:02}:{seconds:02}"
 
     def buy_upgrade(self, upgrade_name: str) -> None:
@@ -154,7 +156,7 @@ class CookieClickerBot:
 
 if __name__ == "__main__":
 
-    # --- test web scraping content --- #
+    # --- debugging --- #
     bot = CookieClickerBot("test_bot", 100)
     print(json.dumps(bot.upgrades, indent=4))
     bot.print_stats()
